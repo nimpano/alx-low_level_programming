33d634a8e4ad72ad608100ebc4a3586b292f9aa7
nimpano <naphtarimpano@gmail.com>
2023-08-21 04:57:26 -0700

    out task updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - main entry point
- * return :always 0 (success)
+ * return: always 0 (success)
  */
 
-int main (void)
+int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
