18e34524042b165e6e02d36a7f64cb8290a87041
nimpano <naphtarimpano@gmail.com>
2023-08-21 04:40:18 -0700

    Hello, puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void)
+/**
+ * printing the first word in programming science
+ */
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
