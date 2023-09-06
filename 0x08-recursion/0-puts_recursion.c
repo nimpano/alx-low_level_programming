5bc2fe26e9ac7230a9b471a9fc42f7f1aca43519
nimpano <naphtarimpano@gmail.com>
2023-09-06 02:16:37 -0700

    string followed by newline

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* main - check code
+ * return: always 0
+ * */
+int main(void)
+{
+	str = ""
+	printf("Enter the string: ");
+	scanf("%d", &str);
+
+	return (0);
+
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* Main file
+ * this is the prototype list in the c 
+ * Return: always return 0
+ */
+void _puts_recursion(char *s);
+
