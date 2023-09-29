4cbcb4c3ed5b45c5b745ccdf16c06a06cc5c2965
nimpano <naphtarimpano@gmail.com>
2023-09-29 02:12:35 -0700

    Data update

---
+++ 0x05-pointers_arrays_strings/learning.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+*Write a function that prints every other character of a string, starting with the first character, followed by a new line.
+*Return: always 0 (success)
+*/
+
+void puts2(char *str);
+	int m;
+	m = *str;
+
+
+char main(void)
+{	int *str;
+	printf("%d\n", m);
+
+}
