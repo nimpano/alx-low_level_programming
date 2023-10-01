5bc14c10ef7b83c8e4da130fdb1253a94a1f89f4
nimpano <naphtarimpano@gmail.com>
2023-10-01 02:20:29 -0700

    Added new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
+#include <stdlib.h>
 /**
  * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
