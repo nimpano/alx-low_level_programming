46fc5174bc50d75a06b6bf524b8122328c81c8ad
nimpano <naphtarimpano@gmail.com>
2023-10-08 04:03:11 -0700

    Its impossible until done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* read_textfile.c - program that read a file and print to POSIX standard output
-* @filename: filename(string pointer)
-* @letter: number of letters printed
-* Return: number of letter on a success
-*/
-
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	unsigned int nread, nwritten;
-	char *buff;
-
-	if (filename == NULL)
-		return (0);
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	buff =  malloc(sizeof(char) * letters);
-
-	nread = read(fd, buff, letters);
-	nwritten = write(STDOUT_FILENO, buff, nread);
-
+	free(buf);
 	close(fd);
-	free(buff);
-	return (nwritten);
+	return (w);
 }
+
