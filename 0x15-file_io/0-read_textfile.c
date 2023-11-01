85a157ba31afb002c0d9b8f08ba0a5dfcd8e1205
nimpano <naphtarimpano@gmail.com>
2023-11-01 00:49:02 -0700

    Done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY );
+	fd = open(filename, O_RDONLY);
 
 	if (fd == STDERR_FILENO)
 		return (0);
